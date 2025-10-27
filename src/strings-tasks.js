85ce907dddccf54b4fe738ead7ddf9ed85caced2
falsyfun <>
2025-10-27 20:23:22 +0300

    feat: add formatTime solution

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -225,8 +225,9 @@ function endsWith(/* str, substr */) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  const timeFormat = (zero) => zero.padStart(2, '0');
+  return `${timeFormat(String(minutes))}:${timeFormat(String(seconds))}`;
 }
 
 /**
